a0b5669967586636f1d5294f9d47a4fbfe1b35d5
tb-dhk <joshuasoh2008@gmail.com>
2024-11-11 23:21:32 +0800

    added option to remove lists and items (resolved #5)

--- src/lists.py
+++ src/lists.py
@@ -126,4 +126,7 @@ def view_list(window, inner_option, selected, removing):
         window.addstr(len(items) + 1, 2, "+ press : to add a new item, e to change list name, r to remove list.", curses.color_pair(4 + (selected[0] == (len(items) + 2))))
         window.addstr(i + 1, 2, symbol + " " + items[item]["name"], curses.color_pair(1 + (selected[0] == i + 2)))
 
-    window.addstr(len(items) + 1, 2, "+ press : to add a new item, e to change list name.", curses.color_pair(4 + (selected[0] == (len(items) + 2))))
+    if removing == ".":
+        window.addstr(len(items) + 1, 2, "+ press r to confirm removal, esc to cancel", curses.color_pair(7))
+    else:
+        window.addstr(len(items) + 1, 2, "+ press : to add a new item, e to change list name, r to remove list.", curses.color_pair(4 + (selected[0] == (len(items) + 2))))

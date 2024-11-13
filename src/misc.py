99113c09326822cf8fc8eb0622b04a7672aee0c2
tb-dhk <joshuasoh2008@gmail.com>
2024-11-13 09:38:13 +0800

    fixed markdown editing and viewing
    - fixed
      - checked if `markdown_content` is empty before adding a new entry
      - set default value for `data` when opening the editor to edit existing entries, handling the `KeyError` when the entry doesn’t exist
    - changed
      - wrapped editor call in `try` block with an empty `except` and added a `finally` block to ensure `markdown_content` is only saved if not empty
    
    - added
      - imported `tempfile` and `subprocess` for handling temporary markdown file creation and display with `glow`
      - added `case "o"` option to open entries with `glow` for preview, using `tempfile.NamedTemporaryFile` to handle temporary file creation
    - fixed
      - corrected log entry message format, replacing `today` with `entry`
    - changed
      - updated `from habits import ...` line to import `ProgressHabit` and `DurationHabit` directly for clearer habit type management
    
    - added
      - optional `data` parameter to `open_editor_and_return_text()` to pass initial text into the temporary file for editing
    - fixed
      - changed `subprocess.call()` to `subprocess.run()` for `editor` execution consistency
      - handled exceptions in `open_editor_and_return_text()` by adding an `Exception` clause that captures the error in `e` for potential debugging

--- src/misc.py
+++ src/misc.py
@@ -3,8 +3,6 @@ import json
 import os
 import tempfile
 import subprocess
-import termios
-import sys
 
 def display_borders(window, selected, split=False, task_list=[]):
     """Draw the border for the task list. If selected[0] >= 2, split the window in half."""
@@ -66,20 +64,21 @@ def coming_soon(window):
     window.addstr(2, 5, "coming soon...")
 
 
-def open_editor_and_return_text(window):
+def open_editor_and_return_text(window, data=""):
     """Open the user's preferred editor to edit a temporary file, then return the edited text."""
     
     with tempfile.NamedTemporaryFile(suffix=".md", delete=False) as tmp_file:
-        tmp_file_path = tmp_file.name
+        tmp_file.write(data.encode('utf-8'))
+        tmp_file_path = tmp_file.name 
     
     try:
         curses.endwin()
         editor = os.getenv('EDITOR', 'lvim')  # Default to nano if not specified
-        subprocess.call([editor, tmp_file_path])  # Run the editor
+        subprocess.run([editor, tmp_file_path])  # Run the editor
         curses.initscr()
         with open(tmp_file_path, 'r') as file:
             content = file.read().strip()
-    except:
+    except Exception as e:
         os.remove(tmp_file_path)
         content = ""
     else:
